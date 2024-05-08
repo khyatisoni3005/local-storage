630b170558d5baeacb09522ec0c6c6d9aee2f4e6
khyatisoni3005 <khyati.soni4035@gmail.com>
2024-05-08 08:53:36 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import LocalStorage from "./LocalStorage"
+import { useEffect, useRef } from "react";
+import { getAllData, saveItem } from "./function/Function";
 
 
 function App() {
+  const email = useRef();
+  const password = useRef();
+
+  const save = () => {
+    const data = {
+      email: email.current.value,
+      password: password.current.value,
+    };
+
+    saveItem(data);
+  };
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
   return (
     <>
-      <LocalStorage />
-
-
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-4">
+            <div className="card shadow">
+              <div className="card-header bg-primary text-light text-center">
+                Add Your Data
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="mb-3">
+                    <label className="form-label">Email :- </label>
+                    <input type="email" className="form-control" placeholder="Enter email" ref={email} />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Password :- </label>
+                    <input type="password" className="form-control" placeholder="Enter password" ref={password} />
+                  </div>
+                  <div className="d-grid gap-2">
+                    <button type="button" className="btn btn-primary" onClick={save}>Save</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
